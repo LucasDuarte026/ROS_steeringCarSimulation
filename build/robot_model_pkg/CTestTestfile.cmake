bee02e7beb1aa7298ace2cb64b4aa15e9763e3c4
Lucas Sales Duarte <106166400+LucasDuarte026@users.noreply.github.com>
2025-06-25 12:50:20 +0200

    Delete build directory

--- build/robot_model_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/joaomarcelo/Documents/IntelligentRobotics/src/robot_model_pkg
-# Build directory: /home/joaomarcelo/Documents/IntelligentRobotics/build/robot_model_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
